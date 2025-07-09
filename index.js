89dc99ce42a108ff8ab5b2b7ca3ff57ec74f0885
Jurrrrr21 <jrprospero21@gmail.com>
2025-07-09 17:05:13 +1000

    Added list of to dos for the chosen day

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const port = "3000";
 app.use(express.urlencoded({extended : true}));
 app.use(express.static("public"));
 
+let toDoList = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
 app.get("/", (req, res) => {
     const view = req.query.view ?? 'today';
+    /*
+    IF they query today or week or month, then we query the database 
+    that contains the list of tasks for that query, e.g.:
+    toDoList = db.query("SELECT * FROM week") if week is what has been selected
+    */
     res.render("index.ejs", {
         view,
         body: '',
-        title: 'ToDoLoo',
+        listOfTasks: toDoList,
     })
 })
 
